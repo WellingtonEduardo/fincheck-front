d60ae505daffb8f9f0944892d7e2c614d7f20dc1
Wellington Eduardo <wellingtoneduardo10@outlook.com>
2024-06-09 07:46:56 -0300

    I modified the login page to have a test email

--- src/view/pages/Login/index.tsx
+++ src/view/pages/Login/index.tsx
@@ -1,58 +1,58 @@
-import {Link} from 'react-router-dom';
-import {Input} from '../../components/Input';
-import {Button} from '../../components/Button';
-import {useLoginController} from './useLoginController';
+import { Link } from 'react-router-dom';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+import { useLoginController } from './useLoginController';
 
 export function Login() {
-	const {
-		handleSubmit,
-		register,
-		errors,
-		isLoading,
-	} = useLoginController();
+  const {
+    handleSubmit,
+    register,
+    errors,
+    isLoading,
+  } = useLoginController();
 
-	return (
+  return (
 
-		<>
-			<header className='flex flex-col items-center gap-4 text-center'>
-				<h1 className='text-2xl font-bold text-gray-900 tracking-[-1px]'>
+    <>
+      <header className='flex flex-col items-center gap-4 text-center'>
+        <h1 className='text-2xl font-bold text-gray-900 tracking-[-1px]'>
           Entre em sua conta
-				</h1>
+        </h1>
 
-				<p className='space-x-2'>
-					<span className='text-gray-700 tracking-[-0.5px]'>
+        <p className='space-x-2'>
+          <span className='text-gray-700 tracking-[-0.5px]'>
             Novo por aqui?
-					</span>
+          </span>
 
-					<Link to='/register' className='tracking-[-0.5px] font-medium text-teal-900'>
+          <Link to='/register' className='tracking-[-0.5px] font-medium text-teal-900'>
             Crie uma conta
-					</Link>
-				</p>
+          </Link>
+        </p>
 
-			</header>
+      </header>
 
-			<form onSubmit={handleSubmit} className='mt-[60px] flex flex-col gap-4'>
-				<Input
-					type='email'
-					placeholder='E-mail'
-					autoComplete='username'
-					error={errors.email?.message}
-					{...register('email')}
-				/>
+      <form onSubmit={handleSubmit} className='mt-[60px] flex flex-col gap-4'>
+        <Input
+          type='email'
+          placeholder='E-mail'
+          autoComplete='username'
+          error={errors.email?.message}
+          {...register('email')}
+        />
 
-				<Input
-					type='password'
-					placeholder='Senha'
-					autoComplete='current-password'
-					error={errors.password?.message}
-					{...register('password')}
-				/>
+        <Input
+          type='password'
+          placeholder='Senha'
+          autoComplete='current-password'
+          error={errors.password?.message}
+          {...register('password')}
+        />
 
-				<Button isLoading={isLoading} type='submit' className='mt-2'>
+        <Button isLoading={isLoading} type='submit' className='mt-2'>
           Entrar
-				</Button>
-			</form >
+        </Button>
+      </form >
 
-		</>
-	);
+    </>
+  );
 }
--- src/view/pages/Login/useLoginController.ts
+++ src/view/pages/Login/useLoginController.ts
@@ -1,44 +1,45 @@
-import {type SigninParams} from './../../../app/services/authService/signin';
-import {zodResolver} from '@hookform/resolvers/zod';
-import {useForm} from 'react-hook-form';
-import {z} from 'zod';
-import {useMutation} from '@tanstack/react-query';
-import {authService} from '../../../app/services/authService';
+import { type SigninParams } from './../../../app/services/authService/signin';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { useMutation } from '@tanstack/react-query';
+import { authService } from '../../../app/services/authService';
 import toast from 'react-hot-toast';
-import {useAuth} from '../../../app/hooks/useAuth';
+import { useAuth } from '../../../app/hooks/useAuth';
 
 const schema = z.object({
-	email: z.string().min(1, 'E-mail é obrigatório').email('Informe um e-mail válido'),
-	password: z.string().min(8, 'Senha deve ter pelo menos 8 dígitos'),
+  email: z.string().min(1, 'E-mail é obrigatório').email('Informe um e-mail válido'),
+  password: z.string().min(8, 'Senha deve ter pelo menos 8 dígitos'),
 });
 
 type FormData = z.infer<typeof schema>;
 
 export function useLoginController() {
-	const {
-		handleSubmit: hookFormHandleSubmit,
-		register,
-		formState: {errors},
-	} = useForm<FormData>({
-		resolver: zodResolver(schema),
-	});
+  const {
+    handleSubmit: hookFormHandleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    defaultValues: { email: 'teste@teste.com', password: '123456789' },
+  });
 
-	const {mutateAsync, isPending} = useMutation({
-		async mutationFn(data: SigninParams) {
-			return authService.signin(data);
-		},
-	});
+  const { mutateAsync, isPending } = useMutation({
+    async mutationFn(data: SigninParams) {
+      return authService.signin(data);
+    },
+  });
 
-	const {signin} = useAuth();
+  const { signin } = useAuth();
 
-	const handleSubmit = hookFormHandleSubmit(async data => {
-		try {
-			const {accessToken} = await mutateAsync(data);
-			signin(accessToken);
-		} catch {
-			toast.error('Credenciais inválidas!');
-		}
-	});
+  const handleSubmit = hookFormHandleSubmit(async data => {
+    try {
+      const { accessToken } = await mutateAsync(data);
+      signin(accessToken);
+    } catch {
+      toast.error('Credenciais inválidas!');
+    }
+  });
 
-	return {handleSubmit, register, errors, isLoading: isPending};
+  return { handleSubmit, register, errors, isLoading: isPending };
 }
